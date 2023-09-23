68e59f1b31bb822233b2281d5e984f082d036cec
EverardoNava <everardo.nava8942@alumnos.udg.mx>
2023-09-22 22:41:27 -0600

    Se agrega la funcion para crear imagenes avif

--- FestivalMusica/gulpfile.js
+++ FestivalMusica/gulpfile.js
@@ -7,6 +7,7 @@ const plumber = require('gulp-plumber');
 const cache = require('gulp-cache');
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
+const avif = require('gulp-avif');
 
 function css(done) {
 
@@ -44,6 +45,18 @@ function versionWebp(done) {
 
     done();
 }
+function versionAvif(done) {
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe(avif(opciones))
+        .pipe(dest("build/img"));
+
+    done();
+}
 
 function dev(done) {
     watch('src/scss/**/*.scss', css);
@@ -52,5 +65,6 @@ function dev(done) {
 exports.css = css;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.dev = parallel(imagenes, versionWebp, dev);
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
 
