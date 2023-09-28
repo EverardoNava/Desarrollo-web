b9a281ddb38f595bc418fe328dd0121ac407539b
EverardoNava <everardo.nava8942@alumnos.udg.mx>
2023-09-28 11:30:01 -0600

    Proyecto finalizado

--- FestivalMusica/gulpfile.js
+++ FestivalMusica/gulpfile.js
@@ -1,23 +1,31 @@
-const { src, dest, watch, parallel } = require("gulp");
-// CSS
-const sass = require("gulp-sass")(require('sass'));
-const plumber = require('gulp-plumber');
+const { src, dest, watch, parallel } = require('gulp');
 
-//Imagenes
+// CSS
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+
+// Imagenes
 const cache = require('gulp-cache');
-const imagemin = require("gulp-imagemin");
-const webp = require("gulp-webp");
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
 const avif = require('gulp-avif');
+
+//JavaScript
+const terser = require('gulp-terser-js')
 
 function css(done) {
 
-    src('src/scss/**/*.scss')// Identificar el archivo de sass
+    src('src/scss/**/*.scss') // Identificar el archivo .SCSS a compilar
+        .pipe(sourcemaps.init())
         .pipe(plumber())
-        .pipe(sass())// Compilarlo
-        .pipe(dest('build/css'));// Almacenarlo en eñ disco duro
-
-
-
+        .pipe(sass()) // Compilarlo
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('build/css')) // Almacenarla en el disco duro
     done(); // callbak que avisa a gulp cuando llegamos al final
 }
 
@@ -57,20 +65,27 @@ function versionAvif(done) {
 
     done();
 }
-function javascript(done){
+function javascript(done) {
     src('src/js/**/*.js')
-    .pipe(dest('build/js'));
+        .pipe(sourcemaps.init())
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('build/js'));
+
+    done();
 }
 
 function dev(done) {
     watch('src/scss/**/*.scss', css);
-    watch('src/scss/**/*.js', javascript);
+    watch('src/js/**/*.js', javascript);
     done();
 }
+
+
 exports.css = css;
 exports.js = javascript;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
-exports.dev = parallel(imagenes, versionWebp, versionAvif,javascript, dev);
+exports.dev = parallel(imagenes, versionWebp, versionAvif, javascript, dev);
 
